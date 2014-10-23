93b8242ca54703b2102060e9c8fa0cc735457384
linorabolini <linorabolini@gmail.com>
2014-10-23 10:07:06 -0300

    recoded LevelViewport

--- js/src/objects/LevelViewport.js
+++ js/src/objects/LevelViewport.js
@@ -1,63 +1,62 @@
 define(function (require){
-	
-	var BaseObject = require('BaseObject'),
-		THREE = require('three'),
-		utils = require('utils'),
-		input = require('input');
 
-	return BaseObject.extend({
+    var BaseObject = require('BaseObject'),
+        THREE = require('three'),
+        utils = require('utils'),
+        input = require('input');
 
-		// variables
+    return BaseObject.extend({
 
-		renderer: null,
-		model: null,
+        // variables
 
-		// functions
-		init: function () {
-			this.__init();
-			input.on("input", this.handleInput);
+        renderer: null,
+        model: null,
+
+        // functions
+        init: function () {
+            this.__init();
+            input.on("input", this.handleInput);
         },
         handleInput: function (event) {
+            if (!this.model) return;
+
             if (event.type == "key") {
                 var key = utils.getKeyCode(event.code);
-                console.log("key " + key + " pressed");
             }
         },
-		addToScreen: function () {
-			document.body.appendChild( this.renderer.domElement );
-		},
-		removeFromScreen: function () {
-			document.body.removeChild( this.renderer.domElement );
-		},
-		startRendering: function (levelModel) {
-			var scope = this;
+        addToScreen: function () {
+            document.body.appendChild( this.renderer.domElement );
+        },
+        removeFromScreen: function () {
+            document.body.removeChild( this.renderer.domElement );
+        },
+        startRendering: function (levelModel) {
+            var scope = this;
 
-			// setup renderer
-			if (window.WebGLRenderingContext)
-				this.renderer = new THREE.WebGLRenderer();
-			else
-				this.renderer = new THREE.CanvasRenderer();
+            // setup renderer
+            if (window.WebGLRenderingContext)
+                this.renderer = new THREE.WebGLRenderer();
+            else
+                this.renderer = new THREE.CanvasRenderer();
 
-			this.renderer.shadowMapEnabled = true;
-			this.renderer.shadowMapType = THREE.PCFShadowMap;
+            this.renderer.shadowMapEnabled = true;
+            this.renderer.shadowMapType = THREE.PCFShadowMap;
 
-			this.renderer.setClearColor(0x00688B, 1);
-			this.renderer.setSize( window.innerWidth, window.innerHeight );
-			window.onresize = function() {
-				scope.renderer.setSize( window.innerWidth, window.innerHeight );
-			};
+            this.renderer.setClearColor(0x00688B, 1);
+            this.renderer.setSize( window.innerWidth, window.innerHeight );
+            window.onresize = function() {
+                scope.renderer.setSize( window.innerWidth, window.innerHeight );
+            };
 
-			this.model = levelModel; // set the level to render
-		},
-		update: function (dt) {
-			this.__update(dt);
-			if(this.model !== null) {
-				this.renderer.render(this.model.scene, this.model.camera);
-			}
-		},
-		dispose: function() {
-			this.__dispose();
-			this.removeFromScreen();
-		}
-	});
+            this.model = levelModel; // set the level to render
+        },
+        update: function (dt) {
+            this.__update(dt);
+            this.model && this.renderer.render(this.model.scene, this.model.camera);
+        },
+        dispose: function() {
+            this.__dispose();
+            this.removeFromScreen();
+        }
+    });
 });
